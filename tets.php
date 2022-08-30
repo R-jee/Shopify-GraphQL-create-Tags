99e7ca7cb3f052c9881de61fbddf6fddd73b755c
Rana Umair <rjee4631@gmail.com>
2022-08-30 13:03:07 +0500

    Update tets.php

--- tets.php
+++ tets.php
@@ -15,7 +15,8 @@
 //     }'
 // ];
 
-$queryUsingVariables = <<<QUERY
+function createTags__withGraphQL( $access_token , $host_shop , $gid , $tags_array ){
+    $queryUsingVariables = <<<QUERY
     mutation addTags(\$id: ID!, \$tags: [String!]!) {
         tagsAdd(id: \$id, tags: \$tags) {
             node {
@@ -26,10 +27,32 @@ $queryUsingVariables = <<<QUERY
             }
         }
     }
-QUERY;
-
-
-$variables = [
-    "id" => "gid://shopify/Product/7325887004864",
-    "tags" => "one, two, three"
-];
+    QUERY;
+    $variables = [
+        "id" => $gid,
+        "tags" => $tags_array
+    ];
+    return shopify_graphQL_call($access_token , $host_shop , "2022-04", ['query' => $queryUsingVariables, 'variables' => $variables] );
+}
+        
+        
+function removeTags__withGraphQL( $access_token , $host_shop , $gid , $tags_array ){
+    $queryUsingVariables = <<<QUERY
+    mutation tagsRemove(\$id: , \$tags: ) {
+        tagsRemove(id: \$id, tags: \$tags) {
+            node {
+                id
+            }
+            userErrors {
+                field
+                message
+            }
+        }
+    }
+    QUERY;
+    $variables = [
+        "id" => $gid,
+        "tags" => $tags_array
+    ];
+    return shopify_graphQL_call($access_token , $host_shop , "2022-04", ['query' => $queryUsingVariables, 'variables' => $variables] );
+}
