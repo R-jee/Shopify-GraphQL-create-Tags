f176c0996224563064d35da338b0db98967792b4
Rana Umair <rjee4631@gmail.com>
2022-08-30 10:54:14 +0500

    Update tets.php

--- tets.php
+++ tets.php
@@ -1,4 +1,20 @@
 
+// $graphql___tags_getAll = [
+//     "query" => 'mutation {
+//         productCreate(
+//             input: {
+//                 title: "Vanilla PHP & GraphQL"
+//                 descriptionHtml: "<p>I am created using GraphQL!</p>"
+//             }
+//         ) {
+//             product {
+//                 id
+//                 title
+//             }
+//         }
+//     }'
+// ];
+
 $queryUsingVariables = <<<QUERY
     mutation addTags(\$id: ID!, \$tags: [String!]!) {
         tagsAdd(id: \$id, tags: \$tags) {
